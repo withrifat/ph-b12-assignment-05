702f251cc7ddc53d1de6438a3749994625e53aac
withrifat <mdrifathassain97880@gmail.com>
2025-08-27 19:28:14 +0600

    add js code for global veriable and make funtion for getelementbyid make easyer

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 
-
-
+// global variables
+let heartCount = 0;
+let copyCount = 0;
+let coinCount = 100;
+// short function code for optimized code 
+function getElement(id) {
+    return document.getElementById(id);
+}
 // heart icon funtionality
 function incrementHeart() {
     const display = document.getElementById('heartValue');
     display.innerText = parseInt(display.innerText || 0) + 1;
 }
+// for call button 
+document.getElementById('callBtn').addEventListener('click', function() {
+    console.log("btn clicked call");
+    
+});
