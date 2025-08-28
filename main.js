614e2881b543b105231b313117ea54d9af02df74
withrifat <mdrifathassain97880@gmail.com>
2025-08-28 14:20:17 +0600

    add code for histry section

--- main.js
+++ main.js
@@ -12,8 +12,20 @@ function incrementHeart() {
     const display = document.getElementById('heartValue');
     display.innerText = parseInt(display.innerText || 0) + 1;
 }
-// for call button 
-document.getElementById('callBtn').addEventListener('click', function() {
-    console.log("btn clicked call");
-    
+
+// copy button section 
+const copyBtns = document.querySelectorAll('#copyBtn');
+for (const btn of copyBtns){
+    btn.addEventListener('click', function(e){
+    const number = e.target.closest("#copyBtn").parentNode.parentNode.children[1].children[2].innerText;
+    copyCount++;
+    getElement("copy-count").innerText = copyCount;
+    navigator.clipboard.writeText(number);
+    alert(`নম্বর কপি হয়েছেঃ ${number}`);
+    });
+}
+
+// clear button functionality
+getElement("btn-clear").addEventListener("click", function () {
+  getElement("history-container").innerHTML = "";
 });
